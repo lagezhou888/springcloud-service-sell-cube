25f72a00f44c08947d15e0d7efc2bdad2a81ec3a
caogzh <243509301@qq.com>
2020-12-04 20:01:05 +0800

    服务发现

---
+++ src/test/java/com/caogzh/SpringbootSellCube1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caogzh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSellCube1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
